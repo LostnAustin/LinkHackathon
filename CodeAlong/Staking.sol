d0e646294ff962a95185975b3b30c507b444b0e1
Chad Lengefeld <36778876+LostnAustin@users.noreply.github.com>
2022-04-27 07:55:39 -0500

    Update Staking.sol

--- CodeAlong/Staking.sol
+++ CodeAlong/Staking.sol
@@ -6,11 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 //for errors
 error Staking__TransferFailed();
+error Staking__NeedsMoreThanZero();
 
 contract Staking {
   IERC20 public s_stakingToken;
 
   mapping(addresss => uint256) public s_balances
+  
+  mapping(address => uint256) public s_userRewardPerTokenPaid;
+  
+  mapping(address => uint256) public s_rewards;
 
   uint256 public s_totalSupply;
   uint256 public s_rewardPerTokenStored;
@@ -21,11 +26,28 @@ contract Staking {
     //define how much per token, last timestamp, periods of time
     s_rewardPerTokenStored = rewardperToken();
     s_lastUpdateTime = block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18 / s_totalSupply);
+    s_rewards[account] = earned(account)
 }
   
-  constructor(address stakingToken) {
-  s_stakingToken = IERC20(stakingToken);
-
+  modifier moreThanZero(uint256 amount) {
+    if( amount === 0) {
+    revert Staking__NeedsMoreThanZero();
+    _;
+  }
+  constructor(address stakingToken, address rewardToken) {
+    s_stakingToken = IERC20(stakingToken);
+    s_rewardToken = IERC@)(rewardToken);
+  }
+  
+  function earned(address account) public view returns(uint256) {
+    uint256 currentBalance = s_balances[account];
+    uint256 amountPaid = s_userRewardPerTokenPaid[account];
+    uint256 currentRewardPerToken = rewardPerToken();
+    
+    uint256 pastRewards =  s_reward[account];
+    
+    (currentBalance * (currentRewardPerToken - amountPaid)) / 1e18) + pastRewards;
+  }
 
   function rewardPerToken public view returns(uint256) {
     if (s_totalSupply == 0 ) {
@@ -33,7 +55,7 @@ contract Staking {
     }
     return s_rewardPerTokenStored + (((block.timestamp
 
-  function stake(uint256 amount) external {
+  function stake(uint256 amount) external updateReward(msg.sender){
   //keep tracker of user, balance, staked amount
 
   s_balances[msg.sender] = s_balances]msg.sender] + amount;
@@ -44,16 +66,18 @@ contract Staking {
   // above same as bool success = s_stakingToken.transferFrom(address(this), msg.sender, amount);
   
   if(!success) {
-  rever Staking__TransferFailed();
+  revert Staking__TransferFailed();
   }
  
  
- function withdraw(uint256 amount) external {
+ function withdraw(uint256 amount) external updateReward(msg.sender){
   s_balances[msg.sender] = s_balances[msg.sender] - amount;
   s_totalSupply = s_totalSupply - amount;
   bool success = s_stakingToken.transfer(msg.sender, amount);
   
-  function claimReward() external {
+  function claimReward() external updateReward(msg.sender){
+    uint256 reward = s_rewards[msg.sender];
+    bool success = s_rewardsToke.transfer
     //how much reward
     //the contract is going to emix X tokens per second and disperse to stakers
     
